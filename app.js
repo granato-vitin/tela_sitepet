ce98e5ddf4b2e06eb639535e9f6143c4abfcd2ff
granato-vitin <victor.granato.oliveira@escola.pr.gov.br>
2026-09-25 10:55:34 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-async function imagemApi() {
-    let respostaApi = await fetch('https://dog.ceo/api/breeds/image/random');
-    let dadosApi = await respostaApi.json();
-    document.querySelector(".img-dog").src = dadosApi.message
-    console.log(dadosApi);
+// async ==> assicrono
+async function imagemApi(){
+// esperar a resposta  ==> await
+let respostaApi = await fetch('https://dog.ceo/api/breeds/image/random');
+let dadosApi = await respostaApi.json();
+document.querySelector('.img-dog').src = dadosApi.message;
+console.log(dadosApi);
 
 }
 imagemApi()
